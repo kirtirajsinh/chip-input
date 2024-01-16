34f5d79565cc69bb73c227b99fc36c0fc52c84d3
kirtirajsinh <kirtiraj3738@gmail.com>
2024-01-16 22:40:17 +0530

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import UserInput from "./components/UserInput";
+
+function App() {
+  return (
+    <>
+      <main className="">
+        <UserInput />
+      </main>
+    </>
+  );
+}
+
+export default App;
